adcb25060f12ed3feb8abbc868bb3bb8198a7d70
dedan <watiri.dedan@eclectics.io>
2025-08-23 11:07:37 +0300

    Feature update: Added bottom button layout and state change management

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,71 @@
+import { images } from "@/constants/images";
+import {icons} from "@/constants/icons";
 import { Tabs } from "expo-router"
 import React from 'react'
+import { ImageBackground, Image, Text, View } from "react-native";
+
+
+const TabIcon = ({focused,title,  icon}: any)=>{
+if(focused){
+    return (
+        <ImageBackground
+                        source={images.highlight}
+                        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+                        >
+                            <Image source={icon} tintColor="#151312" className="size-5"/>
+                            <Text className="text-secondary text-base font-semibold  ml-2">{title}</Text>
+                        </ImageBackground>
+    )
+}
+return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+        <Image source={icon} tintColor='#A8B5DB' className="size-5"/>
+    </View>
+)
+}
 
 const _Layout =()=>{
     return (
         <Tabs>
             <Tabs.Screen
                 name="index"
-                options={{title: 'Home', headerShown:false}}
+                options={{
+                    title: 'Home', 
+                    headerShown:false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon focused={focused} icon={icons.home} title= "Home"/>
+                    )
+                }}
+            />
+
+             <Tabs.Screen
+                name="search"
+                options= {{title:"Search", headerShown:false,
+                    tabBarIcon: ({focused}) =>(
+                        <TabIcon focused={focused} icon={icons.search} title= "Search"/>
+                    )
+
+                }}
             />
 
             <Tabs.Screen
-                name="saved"
-                options= {{title:"Saved", headerShown:false}}
+            name="saved"
+            options={{headerShown:false, 
+                title:"Saved",
+                tabBarIcon:({focused})=>(
+                    <TabIcon focused={focused} icon={icons.save} title="Saved"/>
+                )
+            }}
             />
+
+           
             <Tabs.Screen
-            name="profile"
-            options={{headerShown: false}}/>
+                name="profile"
+                options={{headerShown: false, 
+                    tabBarIcon:({focused})=>(
+                        <TabIcon focused={focused} icon={icons.person} title="Profile"/>
+                    )
+                }}/>
         </Tabs>
     )
 }
