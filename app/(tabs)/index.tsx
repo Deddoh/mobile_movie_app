dd20d0c1bfab7091a61f36b9f74cbe96f6231569
dedan <watiri.dedan@eclectics.io>
2025-08-31 14:45:42 +0300

    Feature update: Added the movie card to containerize the catalogue; Search bar to feature the search input, the filtered catalogue and error handling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -38,6 +38,8 @@ export default function Index() {
       <SearchBar
       onPress={()=> router.push('/search')}
       placeholder = "Search for a movie"
+      value=""
+     
       />
 
       <>
--- services/api.ts
+++ services/api.ts
@@ -10,6 +10,10 @@ export const TMDB_CONFIG = {
 export const fetchMovies = async ({query}:{query: string})=>{
 
     const endpoint = query ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`:`${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`
+
+
+    console.log("query endpoint??", endpoint);
+    
     const response = await fetch(endpoint,{
         method: 'GET',
         headers: TMDB_CONFIG.headers} );
